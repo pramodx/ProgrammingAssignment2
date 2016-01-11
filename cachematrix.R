e45503c79800e625cb2dfc46655f67ee6982eaee
Pramod Nair <pramod.nair@open.edu.au>
2016-01-11 22:24:59 +1100

    added wrapping parentheses cos thats the way I like it.

--- cachematrix.R
+++ cachematrix.R
@@ -10,12 +10,10 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- y
     inv <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)  
+  get <- function() { x }
+  setinverse <- function(inverse) { inv <<- inverse }
+  getinverse <- function() { inv }
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
   
 }
 
